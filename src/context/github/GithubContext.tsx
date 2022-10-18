a40259b34beac994a452ead70f49b9701ca578f8
Thomas Verderesi <tverderesi@gmail.com>
2022-10-18 15:54:45 -0300

    Added correct types and dispatch.

--- src/context/github/GithubContext.tsx
+++ src/context/github/GithubContext.tsx
@@ -4,31 +4,47 @@ import { GHReducer } from './GithubReducer';
 interface AppContextInterface {
   users: any[];
   loading: boolean;
-  fetchUsers: Function;
+  searchUsers: (text: string) => Promise<void>;
+  clearUsers: () => void;
 }
-
-const GHContext = createContext({} as AppContextInterface);
-
-const GH_URL = process.env.REACT_APP_GH_URL;
-const GH_TOKEN = process.env.REACT_APP_GH_TOKEN;
-
 interface Props {
   children?: ReactNode;
   // any props that come into the component
 }
+
+const GHContext = createContext({} as AppContextInterface);
+const GH_URL = process.env.REACT_APP_GH_URL;
+const GH_TOKEN = process.env.REACT_APP_GH_TOKEN;
+
 export const GHProvider = ({ children }: Props) => {
-  const initialState = { users: [], loading: true };
+  const initialState = { users: [], loading: false };
   const [state, dispatch] = useReducer(GHReducer, initialState);
 
-  const fetchUsers = async () => {
-    const res = await fetch(`${GH_URL}/users`, {
+  const searchUsers = async (text: string) => {
+    setLoading();
+    const params = new URLSearchParams({ q: text });
+
+    const test = `${GH_URL}/search/users?${params}`;
+    console.log(test);
+    const res = await fetch(test, {
       headers: { Authorization: `token ${GH_TOKEN}` },
     });
-    const data = await res.json();
-    dispatch({ type: 'GET_USERS', payload: data });
+
+    const { items } = await res.json();
+    console.log(items);
+    dispatch({ type: 'GET_USERS', payload: items });
   };
+
+  //Clear Users from State
+  const clearUsers = () => dispatch({ type: 'CLEAR_USERS' });
+
+  //Set Loading
+  const setLoading = () => dispatch({ type: 'SET_LOADING' });
+
   return (
-    <GHContext.Provider value={{ users: state.users, loading: state.loading, fetchUsers }}>
+    <GHContext.Provider
+      value={{ users: state.users, loading: state.loading, searchUsers, clearUsers }}
+    >
       {children}
     </GHContext.Provider>
   );
