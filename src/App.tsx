236e58f1a56590de3fd01296c9dbb669b922ef4f
Thomas Verderesi <tverderesi@gmail.com>
2022-10-19 17:11:59 -0300

    Added Alert element and user profile route

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { Routes, BrowserRouter as Router, Route } from 'react-router-dom';
 import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
+import Alert from './components/layout/Alert';
 import About from './components/layout/pages/About';
 import Home from './components/layout/pages/Home';
+import User from './components/layout/pages/User';
 import NotFound from './components/layout/pages/NotFound';
 import { AlertProvider } from './context/alert/AlertContext';
 import { GHProvider } from './context/github/GithubContext';
@@ -15,6 +17,7 @@ function App() {
           <div className='flex flex-col justify-between h-screen'>
             <Navbar />
             <main className='container mx-auto px-3 pb-12'>
+              <Alert />
               <Routes>
                 <Route
                   path='/'
@@ -31,6 +34,10 @@ function App() {
                 <Route
                   path='/*'
                   element={<NotFound />}
+                />
+                <Route
+                  path='/user/:login'
+                  element={<User />}
                 />
               </Routes>
             </main>
