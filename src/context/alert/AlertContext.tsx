95cf11d2e8f9ef32e9626ea96f74b5227ad01804
Thomas Verderesi <tverderesi@gmail.com>
2022-10-19 17:13:20 -0300

    formatting and correct typing

--- src/context/alert/AlertContext.tsx
+++ src/context/alert/AlertContext.tsx
@@ -1,8 +1,8 @@
 import { createContext, useReducer, ReactNode } from 'react';
 import alertReducer from './AlertReducer';
 
-interface AlertContextInterface {
-  alert: string | unknown;
+interface AppContextInterface {
+  alert: { msg: string };
   setAlert: (msg: string, type: string) => void;
 }
 interface Props {
@@ -10,17 +10,24 @@ interface Props {
   // any props that come into the component
 }
 
-const AlertContext = createContext({} as AlertContextInterface);
+const AlertContext = createContext({} as AppContextInterface);
 
 export const AlertProvider = ({ children }: Props) => {
+  //
+  //
+  //
   const initialState = null;
   const [state, dispatch] = useReducer(alertReducer, initialState);
+
   const setAlert = (msg: string, type: string) => {
     dispatch({ type: 'SET_ALERT', payload: { msg, type } });
     setTimeout(() => dispatch({ type: 'REMOVE_ALERT' }), 3000);
   };
+
   return (
-    <AlertContext.Provider value={{ alert: state, setAlert }}>{children}</AlertContext.Provider>
+    <AlertContext.Provider value={{ alert: state, setAlert }}>
+      {children}
+    </AlertContext.Provider>
   );
 };
 
